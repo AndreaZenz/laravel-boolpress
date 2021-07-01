9a39a607d28f1dc91df636fd7a3e397e767d471f
AndreaZenz <zenzero.ere@gmail.com>
2021-07-01 18:09:45 +0200

    add admin crud

--- database/migrations/2021_07_01_130900_create_posts_table.php
+++ database/migrations/2021_07_01_130900_create_posts_table.php
@@ -16,7 +16,8 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            
+            $table->text("title");
+            $table->longText("content");
         });
     }
 
